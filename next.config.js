4ce407614828c54d99e7c3f04c675e82a3104625
nicoegiaian <nicolas.egiaian@gmail.com>
2026-06-05 12:45:52 -0300

    Simplify next.config.js by removing unused options
    Removed output and experimental settings from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    appDir: true,
   },
 }
 
